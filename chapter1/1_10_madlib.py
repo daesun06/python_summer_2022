b934bbd3a60c9cb1d9dd25cad8cd3b8484a247d0
Daesun Magay <deson_m@icloud.comls>
2022-06-20 15:47:51 +0600

    My python summer 2022

---
+++ chapter1/1_10_madlib.py
@@ -0,0 +1,6 @@
+adjective = input("Enter a random adjective\n")
+noun = input("Enter a random noun\n")
+verb = input("Enter a random verb\n")
+animal = input("Enter a random animal\n")
+
+print ('The', adjective, noun, verb, 'over the lazy brown', animal)
---
+++ chapter1/1_4_name.py
@@ -0,0 +1,5 @@
+name = input("What is your name?\n")
+
+surname = input("What is your surname?\n")
+
+print("Hi, ", name, surname)
