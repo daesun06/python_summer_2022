e97c820dbab6af4be7bd183bb8d8a116f58f1b50
Daesun Magay <deson_m@icloud.comls>
2022-06-21 13:36:13 +0600

    My python summer 2022 (Chapter 2)

---
+++ chapter2/2_14_squarespiral2.py
@@ -0,0 +1,6 @@
+import turtle
+t = turtle.Pen()
+t.speed(10)
+for x in range(100):
+    t.forward(2*x)
+    t.left(91)
---
+++ chapter2/2_17_colorsquarespiral.py
@@ -0,0 +1,8 @@
+import turtle
+t = turtle.Pen()
+t.speed(0)
+colors = ["red", "yellow", "green", "blue"]
+for x in range(100):
+    t.pencolor( colors[ x % 4])
+    t.forward(2*x)
+    t.left(91)
