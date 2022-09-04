8094c9b3120094ec11829562251b64f1e3a32f02
Deson <deson_m@icloud.com>
2022-09-04 14:33:13 +0600

    Snake Game UPDATE 2
    Minor update. Made the level generation, snake summoming and food spawning into different functions

--- super_snake_game/snake_game.py
+++ super_snake_game/snake_game.py
@@ -11,29 +11,33 @@ delay = 0.1
 backgrounds = ['white', 'gray', 'pink', 'black']
 
 screen = turtle.Screen()
-screen.title("Snake Game")
-screen.bgcolor(random.choice(backgrounds))
-screen.setup(width = 600, height=600)
-screen.tracer(0) # turns off the animations
+
+def level_gen():
+    screen.title("Snake Game")
+    screen.bgcolor(random.choice(backgrounds))
+    screen.setup(width = 600, height=600)
+    screen.tracer(0) # turns off the animations
 
 # snake head
 
 head = turtle.Turtle()
-head.speed(0)
-head.shape("square")
-head.color("red")
-head.penup()
-head.goto(0,0)
-head.direction = "stop"
+def snake_draw(shape:str, color:str):
+    head.speed(0)
+    head.shape(shape)
+    head.color(color)
+    head.penup()
+    head.goto(0,0)
+    head.direction = "stop"
 
 # food that the snake eats W.I.P does nothing just spawns
 
-food = turtle.Turtle()
-food.speed(0)
-food.shape("circle")
-food.color("orange")
-food.penup()
-food.goto(0, 200)
+def food_spawn(shape:str, color:str, x:int, y:int):
+    food = turtle.Turtle()
+    food.speed(0)
+    food.shape(shape)
+    food.color(color)
+    food.penup()
+    food.goto(x, y)
 
 # making the snake controlable
 
@@ -74,13 +78,17 @@ turtle.onkeypress(go_down, "Down")
 turtle.onkeypress(go_left, "Left")
 turtle.onkeypress(go_right, "Right")
 
+# game setup
+
+level_gen()
+snake_draw("square", "red")
+food_spawn("circle", "orange", 80, 200)
+
 # game loop
 
 while True: 
     screen.update() # this makes the screen always update
-
     move() # using my move function to make the snake animated
-
     time.sleep(delay)
 
 turtle.mainloop()
